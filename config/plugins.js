ef6efb9aaae020cbb587be81bdb44d85cf6278e6
Syed Ahsan <68531690+Syed-Ahsan-Naqvi@users.noreply.github.com>
2024-07-05 17:28:43 +0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -15,9 +15,12 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_KEY,
-      api_secret: process.env.CLOUDINARY_SECRET,
+      // cloud_name: process.env.CLOUDINARY_NAME,
+      cloud_name:dbxuynhy0,
+      // api_key: process.env.CLOUDINARY_KEY,
+      api_key:214768323283339,
+      // api_secret: process.env.CLOUDINARY_SECRET,
+      api_secret:5IGw31yNn_50ZkimD8fG2Autrww,
     },
   },
 });
