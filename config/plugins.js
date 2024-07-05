05520c74588651be55a02849d68d5d1857139a36
Syed Ahsan <68531690+Syed-Ahsan-Naqvi@users.noreply.github.com>
2024-07-05 17:17:45 +0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,23 @@
+// module.exports = ({ env }) => ({
+//     upload: {
+//         provider: 'cloudinary',
+//         providerOptions: {
+//             cloud_name: env('CLOUDINARY_NAME'),
+//             api_key: env('CLOUDINARY_KEY'),
+//             api_secret: env('CLOUDINARY_SECRET'),
+//         },
+//     },
+// });
+
+require('dotenv').config();
+
 module.exports = ({ env }) => ({
-    upload: {
-        provider: 'cloudinary',
-        providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
-        },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
     },
+  },
 });
