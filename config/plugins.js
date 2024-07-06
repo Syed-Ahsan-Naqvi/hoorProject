bdb3199823a4daf042a3ec0c5ea6737163d97677
Syed Ahsan <68531690+Syed-Ahsan-Naqvi@users.noreply.github.com>
2024-07-06 10:16:42 +0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -11,6 +11,13 @@
 
 require('dotenv').config();
 
+
+console.log('Cloudinary Config:', {
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
 module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
